e8e6219cb038235cac3df68aee2cf41c7631a4b1
pabskun <pablo.monestel@gmail.com>
2018-01-18 13:02:05 -0600

    add: clase vehiculo

---
+++ ejemploObjetos/js/clases.js
@@ -0,0 +1,10 @@
+class Vehiculo{
+    constructor(pmarca, pmodelo, panno, pcapacidad, pkilometraje){
+        this.marca = pmarca;
+        this.modelo = pmodelo;
+        this.anno = panno;
+        this.capacidad = pcapacidad;
+        this.kilometraje = pkilometraje;
+    }
+
+}
